afc39e48f90ed4b529a4fd5a1a5415557a2cfdf8
Matt Johnson <matt.g.johnson101@gmail.com>
2025-12-26 14:58:45 -0800

    feat: Integrate LAL Dialog into GUI
    - Added 'LALDialog' (UI for managing assets).
    - Added 'Manage Assets (LAL)...' button to Misc tab.
    - Linked LAL backend to ltr_gui.
    - Phase 3 Complete: Users can now access the LAL interface.

---
+++ src/qt_gui/lal_dialog.cpp
@@ -0,0 +1,134 @@
+#include "lal_dialog.h"
+#include <QDebug>
+#include <QFileDialog>
+#include <QHeaderView>
+#include <QMessageBox>
+
+LALDialog::LALDialog(QWidget *parent) : QDialog(parent) {
+  setupUi();
+  refreshTable();
+}
+
+LALDialog::~LALDialog() {}
+
+void LALDialog::setupUi() {
+  setWindowTitle(tr("Licensed Asset Loader (LAL) Beta"));
+  resize(800, 400);
+
+  QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
+  // Info Label
+  QLabel *infoLabel = new QLabel(tr("<b>Licensed Asset Loader</b><br>"
+                                    "Manage proprietary drivers and firmware "
+                                    "required for certain devices.<br>"
+                                    "Assets are installed locally to your user "
+                                    "directory (no Wine required)."),
+                                 this);
+  mainLayout->addWidget(infoLabel);
+
+  // Table
+  assetTable = new QTableWidget(this);
+  assetTable->setColumnCount(5);
+  assetTable->setHorizontalHeaderLabels(
+      {tr("Asset"), tr("Vendor"), tr("Version"), tr("Status"), tr("Action")});
+  assetTable->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
+  assetTable->horizontalHeader()->setSectionResizeMode(
+      4, QHeaderView::ResizeToContents);
+  mainLayout->addWidget(assetTable);
+
+  // Buttons
+  QHBoxLayout *btnLayout = new QHBoxLayout();
+  refreshButton = new QPushButton(tr("Refresh"), this);
+  connect(refreshButton, &QPushButton::clicked, this, &LALDialog::onRefresh);
+
+  closeButton = new QPushButton(tr("Close"), this);
+  connect(closeButton, &QPushButton::clicked, this, &QDialog::accept);
+
+  btnLayout->addStretch();
+  btnLayout->addWidget(refreshButton);
+  btnLayout->addWidget(closeButton);
+  mainLayout->addLayout(btnLayout);
+}
+
+void LALDialog::refreshTable() {
+  auto &mgr = lal::LALManager::instance();
+  // Load manifest if not loaded (prototype path)
+  // TODO: In real app, load from standard location
+  if (mgr.getAssetIds().empty()) {
+    mgr.loadManifest("src/lal/lal_manifest.json"); // Dev path
+  }
+
+  std::vector<std::string> ids = mgr.getAssetIds();
+  assetTable->setRowCount(ids.size());
+
+  int row = 0;
+  for (const auto &id : ids) {
+    auto def = mgr.getAssetDefinition(id);
+    auto status = mgr.getAssetStatus(id);
+
+    assetTable->setItem(row, 0,
+                        new QTableWidgetItem(QString::fromStdString(def.name)));
+    assetTable->setItem(
+        row, 1, new QTableWidgetItem(QString::fromStdString(def.vendor)));
+    assetTable->setItem(
+        row, 2, new QTableWidgetItem(QString::fromStdString(def.version)));
+    assetTable->setItem(row, 3, new QTableWidgetItem(getStatusString(status)));
+
+    QPushButton *actionBtn = new QPushButton(tr("Install"), this);
+    actionBtn->setProperty("asset_id", QString::fromStdString(id));
+    connect(actionBtn, &QPushButton::clicked, this,
+            &LALDialog::onInstallClicked);
+
+    if (status == lal::AssetStatus::INSTALLED) {
+      actionBtn->setText(tr("Reinstall"));
+    }
+
+    assetTable->setCellWidget(row, 4, actionBtn);
+    row++;
+  }
+}
+
+QString LALDialog::getStatusString(lal::AssetStatus status) {
+  switch (status) {
+  case lal::AssetStatus::INSTALLED:
+    return tr("Installed");
+  case lal::AssetStatus::MISSING:
+    return tr("Missing");
+  case lal::AssetStatus::CORRUPT:
+    return tr("Corrupt/Invalid");
+  default:
+    return tr("Unknown");
+  }
+}
+
+void LALDialog::onRefresh() { refreshTable(); }
+
+void LALDialog::onInstallClicked() {
+  QPushButton *btn = qobject_cast<QPushButton *>(sender());
+  if (!btn)
+    return;
+
+  QString assetId = btn->property("asset_id").toString();
+  auto &mgr = lal::LALManager::instance();
+  auto def = mgr.getAssetDefinition(assetId.toStdString());
+
+  // For prototype, ask user for the file
+  // Real LAL will handle downloads or instructions based on 'AssetSource'
+  QString fileName = QFileDialog::getOpenFileName(
+      this, tr("Select Archive for %1").arg(QString::fromStdString(def.name)),
+      QDir::homePath(), tr("Archives (*.zip *.7z *.exe *.tar.gz *.tgz)"));
+
+  if (fileName.isEmpty())
+    return;
+
+  if (mgr.installAssetFromArchive(assetId.toStdString(),
+                                  fileName.toStdString())) {
+    QMessageBox::information(this, tr("Success"),
+                             tr("Asset installed successfully!"));
+    refreshTable();
+  } else {
+    QMessageBox::critical(
+        this, tr("Error"),
+        tr("Failed to install asset. Check console for details."));
+  }
+}
--- src/qt_gui/ltr_gui.h
+++ src/qt_gui/ltr_gui.h
@@ -2,7 +2,7 @@
 #define LTR_GUI__H
 
 #ifdef HAVE_CONFIG_H
-  #include "../../config.h"
+#include "../../config.h"
 #endif
 
 #include <QCloseEvent>
@@ -14,9 +14,9 @@
 class LtrGuiForm;
 class LtrDevHelp;
 class ModelEdit;
-//class LtrTracking;
+// class LtrTracking;
 class LogView;
-//class ScpForm;
+// class ScpForm;
 class QSettings;
 class HelpViewer;
 class PluginInstall;
@@ -25,19 +25,19 @@ class ProfileSelector;
 class Guardian;
 class XPluginInstall;
 
-class LinuxtrackGui : public QWidget
-{
+class LinuxtrackGui : public QWidget {
   Q_OBJECT
- public:
+public:
   LinuxtrackGui(QWidget *parent = 0);
   ~LinuxtrackGui();
- protected:
+
+protected:
   void closeEvent(QCloseEvent *event);
- public slots:
+public slots:
   void show();
- private slots:
+private slots:
   void on_QuitButton_pressed();
-  //void on_EditSCButton_pressed();
+  // void on_EditSCButton_pressed();
   void on_SaveButton_pressed();
   void on_DefaultsButton_pressed();
   void on_DiscardChangesButton_pressed();
@@ -51,16 +51,18 @@ class LinuxtrackGui : public QWidget
   void on_ViewLogButton_pressed();
   void on_XplanePluginButton_pressed();
   void on_PackageLogsButton_pressed();
+  void on_LALButton_pressed();
   void logsPackaged(int exitCode, QProcess::ExitStatus exitStatus);
- private:
+
+private:
   Ui::LinuxtrackMainForm ui;
   LtrGuiForm *showWindow;
   LtrDevHelp *helper;
   DeviceSetup *ds;
   ModelEdit *me;
   Guardian *grd;
-  //LtrTracking *track;
-  //ScpForm *sc;
+  // LtrTracking *track;
+  // ScpForm *sc;
   LogView *lv;
   PluginInstall *pi;
   ProfileSelector *ps;
